e85e531adcfe8483f6776f5a992d73c07111c124
Borna Novak <dosadnizub@gmail.com>
2019-01-06 18:57:42 +0100

    lecture 72

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class ProductPage extends StatelessWidget {
   final String title;
@@ -8,22 +9,29 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Product Detail')),
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.asset('assets/food.jpeg'),
-          Container(padding: EdgeInsets.all(10.0), child: Text('Details!')),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                child: Text('DELETE'),
-                onPressed: () => Navigator.pop(context, true)),
-          )
-        ],
+    return WillPopScope(
+      onWillPop: () {
+        print('Back button pressed');
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(title: Text('Product Detail')),
+        body: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/food.jpeg'),
+            Container(padding: EdgeInsets.all(10.0), child: Text('Details!')),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('DELETE'),
+                  onPressed: () => Navigator.pop(context, true)),
+            )
+          ],
+        ),
       ),
     );
   }
