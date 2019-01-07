2636726a76a099535596ca88066e89404717dc21
Borna Novak <dosadnizub@gmail.com>
2019-01-07 18:22:23 +0100

    lecture 84

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Product Create Page'));
+    return Center(
+      child: RaisedButton(
+        child: Text('Product Create Page'),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return Center(child: Text('Wiii :-D'));
+            },
+          );
+        },
+      ),
+    );
   }
 }
