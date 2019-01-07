5d45399059f5746657b9660d4e7e9d240f0f6f42
Borna Novak <dosadnizub@gmail.com>
2019-01-07 11:11:59 +0100

    lecture 77

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Product Create')),
-      body: Center(child: Text('Product Create Page')),
-    );
+    return Center(child: Text('Product Create Page'));
   }
 }
--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 class ProductListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Product List')),
-      body: Center(child: Text('Product List Page')),
-    );
+    return Center(child: Text('Product List Page'));
   }
 }
