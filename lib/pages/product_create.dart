57b52480effc1843fce57255c5ceee66a0066542
Borna Novak <dosadnizub@gmail.com>
2019-01-09 08:46:07 +0100

    lecture 92

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/pages/products.dart';
 
 class ProductCreatePage extends StatefulWidget {
   final Function addProduct;
@@ -48,8 +49,11 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
               });
             },
           ),
+          SizedBox(height: 10.0),
           RaisedButton(
             child: Text('Save'),
+            color: Theme.of(context).accentColor,
+            textColor: Colors.white,
             onPressed: () {
               final Map<String, dynamic> product = {
                 'title': titleValue,
@@ -58,6 +62,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
                 'image': 'assets/food.jpeg'
               };
               widget.addProduct(product);
+              Navigator.pushNamed(context, '/');
             },
           )
         ],
