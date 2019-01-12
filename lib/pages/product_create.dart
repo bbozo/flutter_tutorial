5b7732c94832da93edd974cfb2e29ca87d93c9c4
Borna Novak <dosadnizub@gmail.com>
2019-01-12 18:08:24 +0100

    lecture 126

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+typedef void _FormValueSetter(String value);
+
 class ProductCreatePage extends StatefulWidget {
   final Function addProduct;
 
@@ -14,8 +16,10 @@ class ProductCreatePage extends StatefulWidget {
 class _ProductCreatePageState extends State<ProductCreatePage> {
   String _title = '';
   double _price = 0.0;
-  String _description = '';
+  String _details = '';
   String _address = '';
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -27,67 +31,93 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
       margin: EdgeInsets.all(10.0),
       child: Container(
         width: targetWidth, // doesnt work with ListView !!!!
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
-          children: <Widget>[
-            _buildTitleTextField(),
-            _buildDescriptionTextField(),
-            _buildAddressTextField(),
-            _buildPriceTextField(),
-            SizedBox(height: 10.0),
-            // GestureDetector(
-            //   onTap: _submitForm,
-            //   child: Container(
-            //     color: Colors.green,
-            //     padding: EdgeInsets.all(5),
-            //     child: Text('My Button'),
-            //   ),
-            // )
-            RaisedButton(
-              child: Text('Save'),
-              onPressed: _submitForm,
-            )
-          ],
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
+            children: <Widget>[
+              _buildTitleTextField(),
+              _buildDescriptionTextField(),
+              _buildAddressTextField(),
+              _buildPriceTextField(),
+              SizedBox(height: 10.0),
+              // GestureDetector(
+              //   onTap: _submitForm,
+              //   child: Container(
+              //     color: Colors.green,
+              //     padding: EdgeInsets.all(5),
+              //     child: Text('My Button'),
+              //   ),
+              // )
+              RaisedButton(
+                child: Text('Save'),
+                onPressed: _submitForm,
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
+  FormFieldSetter<String> _setValue(_FormValueSetter setter) {
+    return ((String value) {
+      setState(() => setter(value));
+      return;
+    });
+  }
+
   Widget _buildTitleTextField() {
-    return TextField(
+    return TextFormField(
       decoration: InputDecoration(labelText: 'Product Title'),
-      onChanged: (String value) => setState(() => _title = value),
+      onSaved: _setValue((String value) => _title = value),
+      validator: (String value) {
+        if (value.isEmpty || value.length < 5) return 'Title is required and should be 5+ characters long';
+      },
     );
   }
 
-  TextField _buildPriceTextField() {
-    return TextField(
-        decoration: InputDecoration(labelText: 'Product Price'),
-        keyboardType: TextInputType.number,
-        onChanged: (String value) =>
-            setState(() => _price = double.parse(value)));
+  Widget _buildPriceTextField() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Product Price'),
+      keyboardType: TextInputType.number,
+      onSaved: _setValue((String value) => _price = double.parse(value)),
+      validator: (String value) {
+        if (value.isEmpty || !RegExp(r'^(?:[1-9]\d*|0)?(?:\.\d+)?$').hasMatch(value)) 
+          return 'Price is required and should be a number';
+      },
+    );
   }
 
-  TextField _buildAddressTextField() {
-    return TextField(
+  Widget _buildAddressTextField() {
+    return TextFormField(
       decoration: InputDecoration(labelText: 'Product Address'),
       maxLines: 4,
-      onChanged: (String value) => setState(() => _address = value),
+      onSaved: _setValue((String value) => _address = value),
+      validator: (String value) {
+        if (value.isEmpty || value.length < 5) return 'Address is required and should be 5+ characters long';
+      },
     );
   }
 
-  TextField _buildDescriptionTextField() {
-    return TextField(
-      decoration: InputDecoration(labelText: 'Product Description'),
+  Widget _buildDescriptionTextField() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Product Details'),
       maxLines: 4,
-      onChanged: (String value) => setState(() => _description = value),
+      onSaved: _setValue((String value) => _details = value),
+      validator: (String value) {
+        if (value.isEmpty || value.length < 5) return 'Details are required and should be 5+ characters long';
+      },
     );
   }
 
-  _submitForm() {
+  void _submitForm() {
+    if (!_formKey.currentState.validate()) return;
+
+    _formKey.currentState.save();
     final Map<String, dynamic> product = {
       'title': _title,
-      'description': _description,
+      'details': _details,
       'address': _address,
       'price': _price,
       'image': 'assets/food.jpeg'
