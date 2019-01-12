c3205c943ab166d157de3c6b782119ee6124f295
Borna Novak <dosadnizub@gmail.com>
2019-01-12 20:30:45 +0100

    lecture 139

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -13,27 +13,36 @@ class ProductListPage extends StatelessWidget {
       itemBuilder: (BuildContext context, int index) {
         final Map<String, dynamic> product = products[index];
 
-        return Column(
-          children: <Widget>[
-            ListTile(
-              leading: CircleAvatar(backgroundImage: AssetImage(product['image'])),
-              title: Text(product['title']),
-              subtitle: Text('\$' + product['price'].toString()),
-              trailing: IconButton(
-                icon: Icon(Icons.edit),
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                    return ProductEditPage(
-                        product: product, updateProduct: updateProduct, productIndex: index);
-                  }));
-                },
+        return Dismissible(
+          key: Key(index.toString()),
+          background: Container(
+            color: Colors.red
+          ),
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading:
+                    CircleAvatar(backgroundImage: AssetImage(product['image'])),
+                title: Text(product['title']),
+                subtitle: Text('\$' + product['price'].toString()),
+                trailing: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () => _productEditAction(context, product, index),
+                ),
               ),
-            ),
-            Divider()
-          ],
+              Divider()
+            ],
+          ),
         );
       },
       itemCount: products.length,
     );
+  }
+
+  void _productEditAction(BuildContext context, Map<String, dynamic> product, int index) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return ProductEditPage(
+          product: product, updateProduct: updateProduct, productIndex: index);
+    }));
   }
 }
